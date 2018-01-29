60b693ef57bcfb3f6fead4504ec2e010eb34b953
Morgan McDermott <morganmcdermott@Morgans-MacBook-Pro.local>
2018-01-28 21:41:28 -0800

    V bumpb

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [
 
 setup(
     name='antenna',
-    version='0.1',
+    version='0.1.2',
     description='Cloud Scraping System',
     author='Morgan McDermott',
     author_email='morganmcdermott@gmail.com',
